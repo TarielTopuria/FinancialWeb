fdfef8c86c020853a761c6585c5811392c9f6440
Tato Topuria <tatotophuria@gmail.com>
2024-01-24 21:13:24 +0400

    add components and routes

---
+++ src/CompanyProfile/CompanyProfile.tsx
@@ -0,0 +1,11 @@
+interface Props {
+    
+}
+
+const CompanyProfile = (props: Props) => {
+  return (
+    <div>CompanyProfile</div>
+  )
+}
+
+export default CompanyProfile
---
+++ src/Components/IncomeStatement/IncomeStatement.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface Props {
+    
+}
+
+const IncomeStatement = (props: Props) => {
+  return (
+    <div>IncomeStatement</div>
+  )
+}
+
+export default IncomeStatement
