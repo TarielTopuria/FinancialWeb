6bd82e576d613728bf183beda4b032586d881e33
Tato Topuria <tatotophuria@gmail.com>
2024-02-01 17:58:02 +0400

    add some components and api calls

--- src/api.tsx
+++ src/api.tsx
@@ -1,38 +1,64 @@
 import axios from "axios"
-import { CompanyProfile, CompanySearch } from "./company";
+import { CompanyIncomeStatement, CompanyKeyMetrics, CompanyProfile, CompanySearch } from "./company";
 
 interface SearchResponse {
     data: CompanySearch[];
 }
 
-export const searchCompanies = async (query:string) => {
-    try{
+export const searchCompanies = async (query: string) => {
+    try {
         const data = await axios.get<SearchResponse>(
             `https://financialmodelingprep.com/api/v3/search?query=${query}&limit=10&exchange=NASDAQ&apikey=YLPabTkG7dvhFLFs7tyZU5c5ypWAXe6R`
         );
         return data;
-    }catch (error){
-        if(axios.isAxiosError(error)){
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
             console.log("Error message: ", error.message);
             return error.message;
-        }else{
+        } else {
             console.log("Unexpected error: ", error);
             return "An unexpected error occured";
         }
     }
 }
 
-export const getCompanyProfile = async (query:string) => {
-    try
-    {
+export const getCompanyProfile = async (query: string) => {
+    try {
         const data = await axios.get<CompanyProfile[]>
-        (
-            `https://financialmodelingprep.com/api/v3/profile/${query}?apikey=${process.env.REACT_APP_API_KEY}`
-        )
+            (
+                `https://financialmodelingprep.com/api/v3/profile/${query}?apikey=YLPabTkG7dvhFLFs7tyZU5c5ypWAXe6R`
+            )
         return data;
     }
-    catch (error: any)
-    {
+    catch (error: any) {
         console.log("Error message form API: ", error.message)
-    } 
+    }
+}
+
+export const getKeyMetrics = async (query: string) => {
+    try {
+        const data = await axios.get<CompanyKeyMetrics[]>
+            (
+                `https://financialmodelingprep.com/api/v3/key-metrics-ttm/${query}?apikey=YLPabTkG7dvhFLFs7tyZU5c5ypWAXe6R`
+            )
+
+        return data;
+    }
+    catch (error: any) {
+        console.log("Error message from getKeyMetrics: ", error.message)
+    }
+}
+
+export const getIncomeStatement = async (query: string) => {
+    try {
+        const data = await axios.get<CompanyIncomeStatement[]>
+            (
+                `https://financialmodelingprep.com/api/v3/income-statement/${query}?period=annual&apikey=YLPabTkG7dvhFLFs7tyZU5c5ypWAXe6R`
+            )
+
+        return data;
+    }
+    catch (error: any) {
+        console.log("Error message from getKeyMetrics: ", error.message)
+    }
 }
